021d99cfae2fb0aab11cdf718fbb32162e492c7b
MunawarShaik <munawarshaik8328@gmail.com>
2023-10-09 19:31:27 +0530

    completed react assignment 4

---
+++ React Assignments/04-Hooks/booklist_4/src/components/WithLogging.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+// Higher Order Component
+function WithLogging(WrappedComponent) {
+  return class extends Component {
+    render() {
+      //  console.log("Rendering BookList...");
+      return <WrappedComponent {...this.props} />;
+    }
+  };
+}
+
+export default WithLogging;
