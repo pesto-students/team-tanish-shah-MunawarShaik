1ee4e68468682a7528e76f308954e91054b23d81
MunawarShaik <munawarshaik8328@gmail.com>
2023-11-25 15:54:56 +0530

    completed node js 6th assignment

---
+++ Node-js Assignments/06_NodeJs_Express_MySQL/wealth_portfolio_app_mysql/models/db.js
@@ -0,0 +1,11 @@
+const mysql = require("mysql");
+const dbConfig = require("../config/config");
+
+var connection = mysql.createPool({
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB,
+});
+
+module.exports = connection;
