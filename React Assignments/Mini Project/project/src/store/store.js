083ea83ca41f55f0b286ff3de9c5702f045c4f1a
MunawarShaik <munawarshaik8328@gmail.com>
2023-11-25 16:04:34 +0530

    working on react 5th and 6th assignments

--- React Assignments/Mini Project/project/src/store/store.js
+++ React Assignments/Mini Project/project/src/store/store.js
@@ -7,30 +7,3 @@ export const store = configureStore({
     auth: authReducer,
   },
 });
-
-// import { persistReducer, persistStore } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
-// import thunk from "redux-thunk";
-
-// import authSlice from "./authSlice";
-
-// const persistConfig = {
-//   key: "root",
-//   storage,
-// };
-
-// const persistedReducer = persistReducer(
-//   persistConfig,
-//   combineReducers({
-//     auth: authSlice,
-//   })
-// );
-
-// const store = configureStore({
-//   reducer: persistedReducer,
-//   middleware: [thunk],
-// });
-
-// const persistor = persistStore(store);
-
-// export { store, persistor };
